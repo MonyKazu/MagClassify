ee9435e3009d38a209c11d20e2bfce17e289e73f
MonyKazu <0av6w84t38t580w@au.com>
2025-06-26 17:37:54 +0900

    Initial Commit

---
+++ MagClassify/MagClassifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagClassifyApp.swift
+//  MagClassify
+//
+//  Created by Kazuya Ishikawa on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MagClassifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MagClassifyTests/MagClassifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagClassifyTests.swift
+//  MagClassifyTests
+//
+//  Created by Kazuya Ishikawa on 2025/06/26.
+//
+
+import Testing
+@testable import MagClassify
+
+struct MagClassifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
